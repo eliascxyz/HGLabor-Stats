0533248f893b029168aa38ab5b9feab330153755
eliascxyz <167119959+EliAS4779@users.noreply.github.com>
2025-02-27 15:31:33 +0100

    Added new font and added LICENSE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+
+	let stats = null;
+
+	onMount(async () => {
+		const response = await fetch(
+			'https://api.hglabor.de/stats/ffa/26a4fcde-de39-4ff0-8ea1-786582b7d8ee'
+		);
+		stats = await response.json();
+	});
+</script>
+
+<h1>Hero FFA Stats</h1>
+
+{#if stats}
+	<pre>{JSON.stringify(stats, null, 2)}</pre>
+{:else}
+	<p>Lade Stats...</p>
+{/if}
